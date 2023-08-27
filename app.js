a9014728a3fffb10a544bb70728ab1847533b9c6
Manidoux41 <lherissondu41@gmail.com>
2023-08-27 10:37:13 +0200

    ajout de commentaires

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
+// suppression de la div à supprimer
 document.querySelector('#a-supprimer').remove();
+
+// style general du body
 let body = document.body;
 body.style.margin = '0';
 body.style.padding = '0';
 body.style.boxSizing = 'border-box';
 body.style.fontFamily = 'Arial';
 
+// Création du Header
 let header = document.createElement('header')
 header.style.backgroundColor = '#e3b04b';
 header.style.width = '100%';
@@ -18,14 +22,15 @@ titre.style.fontSize = '50px';
 titre.textContent = 'Bienvenue';
 header.appendChild(titre);
 
+// Création du sous-menu
 let sousheader = document.createElement('div');
 sousheader.style.backgroundColor = '#abdbe3';
 sousheader.style.padding = '55px'
 let nav = document.createElement('nav');
 nav.style.display = "flex";
-
 let menus = ['Accueil', 'Une autre page']
 
+/* fonction qui gère l'affichage des menus */
 function createMenu(menuArray) {
     for (const item of menuArray) {
         let link = document.createElement('a')
@@ -47,6 +52,7 @@ function createMenu(menuArray) {
 createMenu(menus);
 sousheader.appendChild(nav);
 
+// Création du contenu principal
 let content = document.createElement('div');
 content.style.padding = "25px 45px"
 let paragraphContent = document.createElement("p");
@@ -54,8 +60,7 @@ paragraphContent.innerText = "Ceci est un paragraph créé avec Javascript !"
 content.append(paragraphContent)
 
 
-
-
+// Affichage des 3 éléments header, sous menu et content dans la page principale
 document.body.appendChild(header);
 document.body.appendChild(sousheader);
 document.body.appendChild(content);
